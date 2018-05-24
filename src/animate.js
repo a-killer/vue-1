dfa2ba73ceba202e94d1a25794d2b040e28864c0
killer <angelo@ma-work.co.uk>
2018-05-24 12:19:07 +0100

    testing animejs functions

--- src/animate.js
+++ src/animate.js
@@ -3,6 +3,12 @@ import anime from 'animejs';
 export function translate(element) {
   anime({
     targets: element,
-    translateX: 500
+    translateX: 80,
+    translateY: 20,
+    scale: 1.5,
+    duration: 1000,
+    direction: 'alternate',
+    easing: 'easeOutQuad',
+    loop: true
   });
 }
